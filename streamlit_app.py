5867629bbf7f576dca5fcae0b073d47cc888a6d9
J. Davis <98430977+0xjdavis@users.noreply.github.com>
2024-09-22 12:47:37 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -79,7 +79,9 @@ def get_fetchai_price():
         logger.info(f"API response: {data}")
 
         btc_price_usd = data['bpi']['USD']['rate']
-        console.log(btc_price_usd)
+        
+        os.write(1, btc_price_usd)
+
         
         if data:
             return (f"Current Bitcoin price in USD: {btc_price_usd}")
