d49c2210444fac80a616b19fbc7bcc456114cd0f
J. Davis <98430977+0xjdavis@users.noreply.github.com>
2024-09-22 12:52:33 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -80,8 +80,7 @@ def get_fetchai_price():
 
         btc_price_usd = data['bpi']['USD']['rate']
         
-        
-        os.write(1,b'BTC query was executed.\n')
+        st.toast(btc_price_usd)
 
         
         if data:
